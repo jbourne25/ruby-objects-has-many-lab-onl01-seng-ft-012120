06270c8155591b8edcbbf7a4f7f3df9b2a213c08
Jeremy Bourne <jbourne25@yahoo.com>
2020-02-04 21:23:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ class Artist
   
   attr_accessor :name, :songs 
   
-  @songs = []
+  
   
   
   def initialize(name)
@@ -11,7 +11,7 @@ class Artist
   end 
   
   def songs 
-    @songs << self   
+    @songs << song   
   end 
   
   # def add_song(song) 
